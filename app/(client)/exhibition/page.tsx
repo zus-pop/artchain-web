a64fa93986af5bf1865813294beda9ccb07f6ab6
dduongCongSon <sonduongcong8@gmail.com>
2025-10-17 01:32:44 +0700

    feat: style exhibition

---
+++ app/(client)/exhibition/page.tsx
@@ -0,0 +1,13 @@
+import ImageSlider from '@/components/ImageSlider'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1>Exhibition Page</h1>
+      <ImageSlider />
+    </div>
+  )
+}
+
+export default page
